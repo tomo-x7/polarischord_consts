e8284b91bf71bb09bb23ad8125da09fce0dbc99e
tomo-x7 <158121497+tomo-x7@users.noreply.github.com>
2025-02-20 02:40:40 +0900

    remove verify

--- web/prebuild.ts
+++ web/prebuild.ts
@@ -4,24 +4,11 @@ import path from "node:path";
 import { config } from "dotenv";
 import crypto from "node:crypto";
 config();
-const { GAS_URL: gasurlstr, PRIVATE_KEY } = process.env;
-if (!gasurlstr || !PRIVATE_KEY) throw new Error("env not found");
-const GAS_URL = new URL(gasurlstr);
+const { GAS_URL } = process.env;
+if (!GAS_URL) throw new Error("env not found");
 const isForce = process.argv[2] === "force" || process.env.FORCE === "1";
 if (isForce) console.log("force mode");
 
-const strToU8arr = (str: string) => new Uint8Array(str.split("").map((s) => s.charCodeAt(0)));
-const alg = { name: "RSASSA-PKCS1-v1_5", hash: "SHA-256" };
-const now = new Date();
-const prvkey = await crypto.subtle.importKey("pkcs8", strToU8arr(atob(PRIVATE_KEY)).buffer, alg, false, ["sign"]);
-const signbuf = await crypto.subtle.sign(alg, prvkey, strToU8arr(now.toISOString()));
-const signstr = btoa(
-	Array.from(new Uint8Array(signbuf))
-		.map((v) => String.fromCodePoint(v))
-		.join(""),
-);
-GAS_URL.searchParams.set("data", now.toISOString());
-GAS_URL.searchParams.set("sign", signstr);
 const res = await fetch(GAS_URL).then(async (res) => {
 	if (!res.ok) throw new Error(await res.text());
 	return res.json() as Promise<Res>;
