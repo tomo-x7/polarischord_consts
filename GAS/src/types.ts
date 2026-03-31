76cbba9c1a12cd6af56406a0170239f4a436dbd6
tomo-x7 <158121497+tomo-x7@users.noreply.github.com>
2026-03-31 18:53:20 +0900

    GAS側Polar対応

--- GAS/src/types.ts
+++ GAS/src/types.ts
@@ -3,10 +3,10 @@ export type Raw = RawItem[][];
 
 type DiffConst = { diff: number; const?: number };
 // infがない場合はnull
-type Diffs = { easy: DiffConst; normal: DiffConst; hard: DiffConst; inf: DiffConst | null };
-type Notes = { easy: number; normal: number; hard: number; inf: number | null };
-type Creaters = { easy: string; normal: string; hard: string; inf: string | null };
-type Links = { infVideo?: string; hardVideo?: string; infImage?: string };
+type Diffs = { easy: DiffConst; normal: DiffConst; hard: DiffConst; inf: DiffConst | null, polar: DiffConst | null };
+type Notes = { easy: number; normal: number; hard: number; inf: number | null, polar: number | null };
+type Creaters = { easy: string; normal: string; hard: string; inf: string | null , polar: string | null };
+type Links = { polarVideo?: string; infVideo?: string; hardVideo?: string; infImage?: string };
 
 export type Music = {
 	id: string;
